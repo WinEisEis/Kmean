f5effb814dfdd074ff0f0ac33b001747d9f17881
Chayaphat <xjellomasterx@gmail.com>
2018-05-02 18:50:11 +0700

    kuy

--- kmean.py
+++ kmean.py
@@ -10,6 +10,73 @@ from multiprocessing.dummy import Pool as ThreadPool
 from Initiate import Initiate
 from random import randint
 import time
+from tkinter import *
+import tkinter.messagebox as tm
+
+#GUI
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.label_title = Label(self, text="K-Mean Clustering")
+        
+        
+        #self.mlabel = Label(self,text='My Label')
+        self.label_k = Label(self, text="Number of Centroid (INT or FLOAT): ")
+        self.label_data = Label(self, text="Number of Data (INT): ")
+        self.label_data_proc = Label(self, text="Number of Processors(INT) ")
+        
+
+        self.entry_k = Entry(self)
+        self.entry_data = Entry(self)
+        self.entry_data_proc = Entry(self)
+        
+        self.label_title.grid(row=0)
+        self.label_k.grid(row=1, sticky=E)
+        self.label_data.grid(row=2, sticky=E)
+        self.label_data_proc.grid(row=3, sticky=E)
+        self.entry_k.grid(row=1, column=1)
+        self.entry_data.grid(row=2, column=1)
+        self.entry_data_proc.grid(row=3, column=1)
+
+        #self.checkbox = Checkbutton(self, text="Keep me logged in")
+        #self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="OK", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        # print("Clicked")
+        global k
+        global data
+        global p
+        k = self.entry_k.get()
+        data = self.entry_data.get()
+        p = self.entry_data_proc.get()
+        p = int(p)
+        k = int(k)
+        data = int(data)
+        
+        root.destroy()
+               
+        #if username == "john" and password == "password":
+        #if isinstance(username, str):
+        #    tm.showinfo("Login info", "Welcome John")
+        #    print(username)
+        #else:
+        #    tm.showerror("Login error", "Incorrect username")
+
+
+root = Tk()
+root.geometry('450x150+500+300')
+root.title("K-Mean")
+lf = LoginFrame(root)
+root.mainloop()
+n = data
+#print(k,data)
+
+#Main function
 def FindCenpoint(x,y):
     OfCentroid=0
     mins=1000000.0
@@ -36,8 +103,8 @@ def FindNewCenpoint(x,y):
 
     
 #input k and Point x1-xn
-k = int(input('Input K : '))
-n = int(input('Enter the number of Data :'))
+#k = int(input('Input K : '))
+#n = int(input('Enter the number of Data :'))
 x=[]
 y=[]
 cenpoint=[]
@@ -72,7 +139,7 @@ Initiate.PointPlot(data)
 data = np.array(data)
 x,y = data.T
 #find cenpoint
-pool = ThreadPool()
+pool = ThreadPool(p)
 cenpoint = pool.starmap(FindCenpoint, zip(x,y)) 
 pool.close() 
 pool.join()
@@ -101,7 +168,7 @@ while(flag==0):
         #cenpoint = Initiate.FindNewCenpoint(n,k,data,newcen)
         count += 1
         print("No: ",count)
-        pool = ThreadPool(10)
+        pool = ThreadPool(p)
         cenpoint = pool.starmap(FindNewCenpoint, zip(x,y)) 
         pool.close() 
         pool.join()
